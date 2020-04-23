bfb97efa0acee9752f59ecaa6ecbcfbaf998f170
COULLON CAMIL p1804059 <camil.coullon@etu.univ-lyon1.fr>
2020-04-23 11:02:33 +0200

    modif navbar +ajout accueil

--- views/v_header.php
+++ views/v_header.php
@@ -23,17 +23,14 @@
 		<!-- En-tête -->
 		<header>
 			<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-				<a class="navbar-brand" href="#">
-					<img src="<?= PATH_LOGO ?>" width="30" class="d-inline-block align-top mb-3 mx-2" alt="">
-					NomPortail
-				</a>
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
+				<img src="<?= PATH_LOGO ?>" width="50" class="d-inline-block align-center mb-3 mx-2" alt="">
 				<div class="collapse navbar-collapse" id="navbarNav">
 					<ul class="navbar-nav">
 					<li class="nav-item mx-3">
-						<a class="nav-link" href="#">Toutes les publications <span class="sr-only">(current)</span></a>
+						<a class="nav-link" href="index.php">Accueil<span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item mx-3">
+						<a class="nav-link" href="#">Toutes les publications</span></a>
 					</li>
 					<li class="nav-item mx-3">
 						<a class="nav-link" href="#">Mes publications</a>
@@ -46,7 +43,6 @@
 					</li>
 					</ul>
 					<a href="index.php?page=connexion"><button class="btn btn-outline-light my-3 float-right" type="button" <?php echo ($page=='connexion' ? '':'')?>>Se connecter</button></a>
-					
 				</div>
 			</nav>
 		</header>
