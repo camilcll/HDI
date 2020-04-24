72a32526bc199b66b5462a70fae27f1b669d905e
COULLON CAMIL p1804059 <camil.coullon@etu.univ-lyon1.fr>
2020-04-24 11:29:21 +0200

    ajout license footer

--- views/v_footer.php
+++ views/v_footer.php
@@ -1,15 +1,15 @@
 					<!-- fin de page -->
 			</section>
 		<!-- Pied de page -->
-		<div class="footer">
-			<div class="container" >
-				<div class = "row">
-					<div class = "col-md-3 col-sm-6 col-xs-12">
-						<!--<img src="assets/images/logoTennis.png" style="width:25%;">-->
-						<h3>Copyright</h3>
-					</div>
-				</div>
+		<footer class=" bg-dark d-flex justify-content-center align-items-center p-3">
+			<div class="d-flex justify-content-center align-items-center flex-column">
+				<a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank">
+					<img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" />
+				</a>
+				<p class="text-white m-3">This work is licensed under a 
+					<a class="text-white" rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank">Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.</a>
+				</p>
 			</div>
-		</div>
+		</footer>
 	</body>
 </html>	
