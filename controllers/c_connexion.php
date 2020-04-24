3b1a9d2efb942a3aa9fb8a14fb6abb3376905b05
BEN HAMIDA HANA p1804713 <hana.ben-hamida@etu.univ-lyon1.fr>
2020-04-24 14:59:01 +0200

    plus d erreurs si champs de connexion sont vident

--- controllers/c_connexion.php
+++ controllers/c_connexion.php
@@ -24,22 +24,24 @@ require_once(PATH_ENTITY . 'Chercheur.php');
 }*/
 
 
-if (isset($_POST['email']))
+if (isset($_POST['email']) && !empty($_POST['email']))
 {
 	$EMAIL = htmlspecialchars($_POST['email']);
 	//si l'utilisateur est l'administrateur alors variable session admin passe a true
 	
+	if (isset($_POST['password']) && !empty($_POST['password']))
+	{
+			$uDao = new UserDAO();
+			$userBD = $uDao->getUserByUsername($EMAIL);
+			if (!$userBD) $alert=choixAlert('erreur_id');
+			if (!isset($_POST['password']) || $_POST['password']!=$userBD->getPwd()) $alert = choixAlert('erreur_mdp');
+			else {
+				$alert = choixAlert('ok_connexion');
+				$_SESSION['logged'] = true;
+			}
 
-		 $uDao = new UserDAO();
-		$userBD = $uDao->getUserByUsername($EMAIL);
-		if (!$userBD) $alert=choixAlert('erreur_id');
-		if (!isset($_POST['password']) || $_POST['password']!=$userBD->getPwd()) $alert = choixAlert('erreur_mdp');
-		else {
-			$alert = choixAlert('ok_connexion');
-			$_SESSION['logged'] = true;
+			//$identite=$userBD->getUsername();
 		}
-
-		//$identite=$userBD->getUsername();
   
 }
 
