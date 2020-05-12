6a747a81cb471c61c1c5867104fbc6053babb4df
COULLON CAMIL p1804059 <camil.coullon@etu.univ-lyon1.fr>
2020-05-12 16:27:02 +0200

    ajout du script qui remplit les dernieres publis

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -35,3 +35,20 @@ function sendTrash(id) {
         return;
     }     
 }
+
+$(document).ready(function () {
+    var dt = new Date();
+    var oldDate = (dt.getFullYear() - 1) + '-' + (dt.getMonth()+1) + '-' + dt.getDate();
+    var newDate = dt.getFullYear() + '-' + (dt.getMonth()+1) + '-' + dt.getDate();
+    console.log(oldDate);
+    console.log(newDate);
+    
+    $.getJSON('https://api.archives-ouvertes.fr/search/?q=collCode_s:LTDS%20AND%20docType_s:*%20AND%20NOT%20popularLevel_s:1%20AND%20(producedDate_tdate:[' + oldDate + 'T00:00:00Z%20TO%20' + newDate + 'T00:00:00Z]%20OR%20publicationDate_tdate:[' + oldDate +'T00:00:00Z%20TO%20'+newDate+'T00:00:00Z])%20AND%20submittedDate_tdate:['+oldDate+'T00:00:00Z%20TO%20'+newDate+'T00:00:00Z]&rows=10&fl=*&sort=submittedDate_tdate%20desc&wt=json', function (data) {
+        console.log($(data.response.docs)[0]);
+        (data.response.docs).forEach(element => {
+            $("#publi_container").append('<div class="publi m-4 rounded-lg p-3">' + element["citationFull_s"] + '.' +'</div>')
+        });
+        
+    });
+
+});
--- views/v_accueil.php
+++ views/v_accueil.php
@@ -16,8 +16,9 @@ if( isset($_SESSION['id']))
   }
 }
 
-?>
+//halId_s files_s citationFull_s abstract_s producedDate_s
 
+?>
 
 <a href="#publi_container"><button id="scrllDown" type="button" class="btn btn-dark">Voir les dernières publications</button></a>
 
@@ -31,22 +32,13 @@ if( isset($_SESSION['id']))
     <div id="aside_cont" class="d-flex justify-content-center align-items-center flex-column rounded-lg m-3">
       <a href="https://hal.archives-ouvertes.fr/LTDS/" class="d-flex justify-content-center align-items-center flex-column">
         <h3 class="text-dark">Publication du LTDS</h3>
-        <img src="<?= PATH_IMAGES ?>/logo_hal.jpg" alt="">
+        <img src="<?= PATH_IMAGES ?>logo_hal.jpg" alt="">
       </a>
     </div>  
   </aside>
   <div id="publi_container" class="d-flex justify-content-center align-items-center flex-column ml-3">
     <h1>Dernières publications</h1>
-    <div class="publi m-4 rounded-lg p-3">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Inventore fugiat autem eveniet molestiae sint corporis, perspiciatis officiis, quia soluta, illum nisi reiciendis iure recusandae harum corrupti veritatis omnis! Porro, ipsum?</div>
-    <div class="publi m-4 rounded-lg p-3">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus repudiandae, culpa libero fugiat dolorem iusto accusantium quidem laborum eaque nemo reprehenderit voluptates consectetur laboriosam, corrupti ab dolore animi doloribus vero.</div>
-    <div class="publi m-4 rounded-lg p-3">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reprehenderit, ex exercitationem. Facere assumenda molestiae ipsa corrupti commodi dolorem, rem ex vero sapiente ab deleniti maxime harum accusamus tempore. Aperiam, doloribus!</div>
-    <div class="publi m-4 rounded-lg p-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab nisi corporis, nihil fuga veritatis, possimus tenetur architecto cumque voluptatibus, corrupti dolorem porro at! Nisi, aut. Cumque assumenda suscipit cupiditate recusandae.</div>
-    <div class="publi m-4 rounded-lg p-3">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga, neque amet. Vero, deserunt sit placeat perspiciatis facere quo doloribus quae commodi reiciendis deleniti officiis, labore doloremque repudiandae autem? Expedita, perspiciatis!</div>
-    <div class="publi m-4 rounded-lg p-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet voluptatem quidem nisi optio incidunt, quam amet perferendis tempora quas cum deleniti quae totam est, odit impedit nemo blanditiis. Alias, rerum!</div>
-    <div class="publi m-4 rounded-lg p-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus deleniti pariatur tenetur nostrum dolores ipsum qui quibusdam similique odit officia eligendi reprehenderit ducimus alias possimus molestias, ea, ad cumque iusto?</div>
-    <div class="publi m-4 rounded-lg p-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quasi nulla eligendi sequi nihil, eum ipsa corporis ab repellendus sint ratione nisi non dolor voluptates natus explicabo corrupti minima voluptatum qui.</div>
-    <div class="publi m-4 rounded-lg p-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, repellendus similique? Excepturi, veniam error? Doloribus fuga totam odit provident fugiat saepe quam, sit, aliquam eos, fugit recusandae voluptate beatae aliquid?</div>
-    <div class="publi m-4 rounded-lg p-3">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque rerum odit saepe obcaecati iste incidunt voluptates repudiandae sint consequuntur officia et ea voluptate quisquam praesentium culpa expedita, quidem eum fugit!</div>
+    
   </div>
 </div>
 
