f592952a637429ff9108cffda2fb1d8d2d5aed51
COULLON CAMIL p1804059 <camil.coullon@etu.univ-lyon1.fr>
2020-05-19 12:34:49 +0200

    changement couleur selection publis

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -66,9 +66,10 @@ function updateTtPub(data = null) {
             $.each(data, function (index, item) {
                 dataHtml += '<div id="'+ item.id +'" class="publi m-4 rounded-lg p-3" onclick="selectPub(this)">' + item.citation + '</div>';
             });
-
-            $("#publi_container_tt").html(dataHtml);
+            $("#publi_container_tt").children().hide();
+            $("#publi_container_tt").append(dataHtml);
             console.log(data);
+            console.log(pagination);
         }
     })
 };
@@ -80,7 +81,7 @@ function updateLastPub() {
 
     $.getJSON('https://api.archives-ouvertes.fr/search/?q=collCode_s:LTDS%20AND%20docType_s:*%20AND%20NOT%20popularLevel_s:1%20AND%20(producedDate_tdate:[' + oldDate + 'T00:00:00Z%20TO%20' + newDate + 'T00:00:00Z]%20OR%20publicationDate_tdate:[' + oldDate + 'T00:00:00Z%20TO%20' + newDate + 'T00:00:00Z])%20AND%20submittedDate_tdate:[' + oldDate + 'T00:00:00Z%20TO%20' + newDate + 'T00:00:00Z]&rows=10&fl=*&sort=submittedDate_tdate%20desc&wt=json', function (data) {
         (data.response.docs).forEach(element => {
-            $("#publi_container").append('<div id="' + element['halId_s']+ '" class="publi m-4 rounded-lg p-3">' + element["citationFull_s"] + '.' + '</div>')
+            $("#publi_container").append('<div id="' + element['halId_s'] + '" class="publi m-4 rounded-lg p-3" onclick="selectPub(this)">' + element["citationFull_s"] + '.' + '</div>')
         });
 
     });
