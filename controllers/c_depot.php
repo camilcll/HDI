70d41d35400e51c6fa46886b19d908df5ef806f7
COULLON CAMIL p1804059 <camil.coullon@etu.univ-lyon1.fr>
2020-06-04 12:42:10 +0200

    ajout fonction sendPub dans c_depot

--- controllers/c_depot.php
+++ controllers/c_depot.php
@@ -4,6 +4,36 @@ if (isset($erreur)) {
     $alert = choixAlert($erreur);
 }
 
+function sendPub($xml){
+    $file = file_get_contents($xml, true);
+
+    // create a new cURL resource
+    $ch = curl_init();
+    //print_r(curl_getinfo($ch));
+    curl_setopt($ch, CURLOPT_URL, "https://api-preprod.archives-ouvertes.fr/sword/hal/");
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,TRUE);
+    //curl_setopt($ch, CURLOPT_VERBOSE, TRUE);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER,FALSE);
+    curl_setopt($ch, CURLOPT_POST, TRUE);
+    $headers = array("Content-Type:text/xml",
+    "Authorization: Basic YWRtaW4tcG9ydGFpbDphZG1pbi1wb3J0YWls", 
+    "Packaging:http://purl.org/net/sword-types/AOfr",
+    );
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $file);
+    $response = curl_exec($ch);
+    print_r("reponse:".$response."\n\n\n");
+    print_r(curl_getinfo($ch));
+    echo "\nErrno:";
+    print_r(curl_errno($ch));
+    echo "\nError:";
+    print_r(curl_error($ch));
+
+    curl_close($ch);
+}
+    
+//sendPub("./test.xml");
+
 
 
 if(isset($_POST['title']) OR isset($_POST['journal']) OR isset($_POST['date-article']) OR isset($_POST['searchAuthor']) OR isset($_POST['researchData']) OR isset($_POST['bookTitle']) OR 
